3da52105b2c3213a6f87785b92b5b40f445fe8bf
Fred <elias5401@hotmail.fr>
2021-01-10 00:21:35 +0100

    Final fixes

--- src/index/App.vue
+++ src/index/App.vue
@@ -25,7 +25,13 @@
           <h3 class="my-4 text-center">Bienvenue sur mon site !</h3>
           <hr class="custom">
           <p class="p-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae veritatis delectus qui, facere molestiae officiis tempora? Vero laboriosam facere deserunt, nobis accusamus corrupti consequuntur veniam, ipsum assumenda maxime sit rerum.
+            Bonjour à tous et bienvenue sur mon site web! <br>
+            Je suis un jeune développeur full-stack passionné d'informatique.<br>
+            J'ai développé ce site internet dans le but de recenser mes différents projets personnels et créations artistiques. <br><br>
+            Ce site internet était précédemment développé en HTML pur, mais avec le temps j'ai pris la décision de développer une nouvelle version avec <a href="https://vuejs.org/" target="_blank">VueJS</a>. <br>
+            J'appliquerai donc au fil du temps des mise à jour afin de l'améliorer. Tout comme la version précédente, le site est bien entendu responsive (et moins buggué de ce côté là).<br><br>
+            Il se peut certains liens dans <a href="projets.html">PROJETS</a> soient mort, ceci étant malheureusement indépendant de ma volonté.<br>
+            Bonne visite ! :D
           </p>
         </b-col>
       </b-row>
--- src/projets/App.vue
+++ src/projets/App.vue
@@ -23,7 +23,7 @@
       <b-row>
         <b-col>
           <ProjectItem projectName="Mon site web" url="#" backgroundURL="/assets/elias_site_picture.png"/>
-          <ProjectItem projectName="lawBOT - Discord BOT" url="https://law.minecraftforgefrance.fr/" backgroundURL="https://law.minecraftforgefrance.fr/images/logo.png"/>
+          <ProjectItem projectName="lawBOT - Discord BOT" url="https://law.minecraftforgefrance.fr/" backgroundURL="/assets/lawbot.png"/>
           <ProjectItem projectName="Admin Weapons - Minecraft Mod" url="https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2232900-1-7-10-1-14-4-admin-weapons-v3-6-last-update" backgroundURL="https://i.imgur.com/ikWxzLm.png"/>
           <ProjectItem projectName="Fake Ores - Minecraft Mod" url="https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2461487-1-7-10-1-13-2-fake-ores-ores-become-alive-now" backgroundURL="https://i.imgur.com/3SxLGpN.png"/>
           <ProjectItem projectName="Creeperade - Minecraft Mod" url="https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2732815-1-10-1-13-2-creeperade-new-weapons-food-and-more" backgroundURL="https://static.planetminecraft.com/files/resource_media/screenshot/1503/2015-01-12_2129028530933_lrg.jpg"/>
